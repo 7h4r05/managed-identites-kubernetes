d2968c64373b8c24a11cf11d069346915d181503
Dariusz Zabrzeński <dariusz.zabrzenski@gmail.com>
2019-01-03 01:18:36 +0100

    Remove console.log

--- todo-keyvault/client.factory.js
+++ todo-keyvault/client.factory.js
@@ -1,7 +1,7 @@
 const KeyVault = require('azure-keyvault');
 const axios = require('axios');
 
-key_vault_url = 'https://todo-kv.vault.azure.net';
+key_vault_url = 'https://{VAULT_NAME}.vault.azure.net';
 ad_url ='http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net';
 
 authenticator = async function(challenge, callback){
@@ -11,7 +11,6 @@ authenticator = async function(challenge, callback){
         }
     });
     const authorizationValue = `${response.data.token_type} ${response.data.access_token}`;
-    console.log(authorizationValue);
     return callback(null, authorizationValue);
 }
 
